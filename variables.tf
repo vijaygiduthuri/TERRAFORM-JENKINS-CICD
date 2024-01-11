bec51f174b6a6aa7160604da76310ac5d8c9f1ab
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2024-01-11 16:51:31 +0530

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "ap-south-1"
+}
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     = "Mumbai"     #change key name here
+}
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     = "t2.medium"
+}
+variable "ami_id" {
+  description = "AMI for Ubuntu Ec2 instance"
+  default     = "ami-0f5ee92e2d63afc18"
+}
+variable "bucketname" {
+  description = "The name of the S3 bucket to create"
+  type        = string
+  default     = "ajaykumar-yegireddi-cloud"  #change Bucket name also
+}
