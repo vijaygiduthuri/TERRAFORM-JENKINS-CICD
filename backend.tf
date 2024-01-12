bce44ea84308ab4d27380ab0d516dd17e90f025e
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2024-01-12 12:59:16 +0530

    backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "mys3bucketforproject"          #change according to s3 bucket name
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
-    dynamodb_table = "mrcloudbook-dynamo-db-table"   #change dynamodb table name
+    dynamodb_table = "vijaygiduthuri-dynamo-db-table"   #change dynamodb table name
   }
 }
