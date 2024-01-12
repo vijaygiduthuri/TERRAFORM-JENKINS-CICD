b9bf75469ae4f67252d7ef5ef146f6cdb088cf92
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2024-01-13 00:35:00 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mys3bucketforproject"          #change according to s3 bucket name
+    bucket         = "s3buckerforterraform"          #change according to s3 bucket name
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
     dynamodb_table = "vijaygiduthuri-dynamo-db-table"   #change dynamodb table name
