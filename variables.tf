9cd87c1fef422a73877a2cb7ee3079e8463bc734
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2024-01-12 13:08:46 +0530

    variables.tf

--- variables.tf
+++ variables.tf
@@ -17,5 +17,5 @@ variable "ami_id" {
 variable "bucketname" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "ajaykumar-yegireddi-cloud"  #change Bucket name also
+  default     = "vijay-giduthuri-cloud"  #change Bucket name also
 }
