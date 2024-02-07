af8d45b03b8142a3ab547a6fcb8551f150a0258d
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2024-02-07 21:44:55 +0530

    backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "mys3bucketforterraform"            # create S3 Buket & Copy&Paste s3 bucket name here
+    bucket         = "mys3bucketforterraform"            # create S3 Buket & Copy&Paste s3 bucket name here   
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
-    dynamodb_table = "vijaygiduthuri-dynamo-db-table"    # create dynamodb table & Copy&Paste dynamodb table name here
+    dynamodb_table = "vijaygiduthuri-dynamo-db-table"    # create dynamodb table & Copy&Paste dynamodb table name here 
   }
 }
